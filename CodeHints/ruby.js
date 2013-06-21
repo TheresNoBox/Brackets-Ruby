9243114b474c6c4497cec7396e1c739aa18ab08f
Bill Centinaro <Bill@theresnobox.net>
2013-06-21 16:45:29 -0700

    More basic stucture.

--- CodeHints/ruby.js
+++ CodeHints/ruby.js
@@ -5,4 +5,27 @@ define(function (require, exports, module) {
 		CodeHintManager   = brackets.getModule("editor/CodeHintManager"),
         Async             = brackets.getModule("utils/Async"),
         StringUtils       = brackets.getModule("utils/StringUtils");
+    
+    function RubyHints() {
+		this.primaryTriggerKeys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+		this.secondaryTriggerKeys = ".(";
+	}
+    
+    RubyHints.prototype.hasHints = function (editor, implicitChar) {
+		this.editor = editor;
+        this.currentText = "";
+		var result = true;
+
+		if (implicitChar) {
+			result = (this.primaryTriggerKeys.indexOf(implicitChar) !== -1) ||
+				(this.secondaryTriggerKeys.indexOf(implicitChar) !== -1);
+		}
+		return result;
+	};
+    
+    RubyHints.prototype.getHints = function (implicitChar) {
+        
+        return 'test';
+    };
+
 });
