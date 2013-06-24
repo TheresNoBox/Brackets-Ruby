7b4ee0793bf5d158690ce5a6a469e43fdc3dd331
Bill Centinaro <Bill@theresnobox.net>
2013-06-24 14:29:38 -0700

    Ported over some code cleanup

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-/*
-    Created by Edwin van Koppen, W3S.nl 2013
-*/
 /*global define, $, brackets, window */
 define(function (require, exports, module) {
-  "use strict";
-  var LanguageManager = brackets.getModule("language/LanguageManager"),
-      language = LanguageManager.getLanguage("html");
+    "use strict";
   
-  language.addFileExtension("erb");
+    var LanguageManager = brackets.getModule("language/LanguageManager"),
+        erb = LanguageManager.getLanguage("html"),
+        ruby = LanguageManager.getLanguage("ruby");
   
-  LanguageManager.defineLanguage("haml", {
-    name: "Haml",
-    mode: "haml",
-    fileExtensions: ["haml"],
-    blockComment: ["-#", "  "],
-    lineComment: ["-#"]
-  });
-
-  language = LanguageManager.getLanguage("ruby");
-  language.setLineCommentSyntax("#");  
+    erb.addFileExtension("erb");
+    ruby.setLineCommentSyntax("#");
+  
+    LanguageManager.defineLanguage("haml", {
+        name: "Haml",
+        mode: "haml",
+        fileExtensions: ["haml"],
+        blockComment: ["-#", "  "],
+        lineComment: ["-#"]
+    });
 });
